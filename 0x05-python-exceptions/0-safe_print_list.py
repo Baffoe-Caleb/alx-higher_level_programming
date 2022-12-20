cff565b7558c3f7a6066bbaa769ecd7509d570e4
Baffoe-Caleb <https://github.com/Baffoe-Caleb/>
2022-12-20 21:35:55 +0000

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
     count = 0
 
@@ -12,4 +10,4 @@ def safe_print_list(my_list=[], x=0):
         else:
             count += 1
     print()
-    return
+    return (count)
